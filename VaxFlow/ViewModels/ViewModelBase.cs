30351d220e7dc23eb9262a546cb635d2a0fa738e
Перкин Евгений <evgenii.perkin@outlook.com>
2025-09-11 20:42:45 +0300

    Добавьте файлы проекта.

---
+++ VaxFlow/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VaxFlow.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ VaxFlow/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VaxFlow.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
