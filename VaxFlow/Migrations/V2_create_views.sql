9c8454e4d76777777a0b10a9a07e886aadda0e0e
Перкин Евгений <evgenii.perkin@outlook.com>
2025-09-21 13:00:28 +0300

    добавил вью для отображения партий вакцин

---
+++ VaxFlow/Migrations/V2_create_views.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW IF NOT EXISTS party_summary AS
+SELECT 
+    p.Id,
+    p.count,
+    p.dt_create,
+    p.party_name,
+    v.desc as vaccine_name,
+    vv.version as vaccine_version
+FROM parties p
+INNER JOIN vaccines v ON p.vaccine_id = v.id
+INNER JOIN vaccine_versions vv ON p.vaccine_version_id = vv.id;
