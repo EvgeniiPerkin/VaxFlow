2079891b3c19a7c72d38bb9f82dd92f530836e32
Перкин Евгений <evgenii.perkin@outlook.com>
2025-09-27 19:47:07 +0300

    добавил 2 триггера на операции создания и удаления записи вакцинации (де\инкремент кол-ва вакцин)

---
+++ VaxFlow/Migrations/V4_create_triggers.sql
@@ -0,0 +1,17 @@
+﻿CREATE TRIGGER after_vaccinations_delete
+AFTER DELETE ON vaccinations
+FOR EACH ROW
+BEGIN
+    UPDATE vaccines 
+    SET count = count + 1
+    WHERE id = OLD.vaccine_id;
+END;
+
+CREATE TRIGGER after_vaccinations_insert
+AFTER INSERT ON vaccinations
+FOR EACH ROW
+BEGIN
+    UPDATE vaccines 
+    SET count = count - 1
+    WHERE id = NEW.vaccine_id;
+END;
